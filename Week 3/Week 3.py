758b88bebabb8919556e8171264ca456d7745905
kmp16101 <klauss.preising@uconn.edu>
2018-09-27 11:01:15 -0400

    Edit week 3

--- Week 3/Week 3.py
+++ Week 3/Week 3.py
@@ -1,5 +1,18 @@
-def change(n):
-    coins =  [0.5, 0.25, 0.1, 0.05, 0.01]
+from random import *
 
-    for i in coins:
-        pass
+"""
+Third Weekly Challenge:
+Given this list [25, 50, 10, 5, 1] of coins find the least amount of coins it would take to give change for.
+Example:
+201 would be 5 coins because 4 50 cent coins and 1 1 cent coins
+23 would be 5 coins because 2 30 cent coins and 3 1 cent coins
+
+"""
+
+def change(n):
+    coins = [50, 25, 10, 5, 1]
+
+
+for i in range(5):
+    temp = int(1000*random())
+    print(temp, "change =", change(temp))
