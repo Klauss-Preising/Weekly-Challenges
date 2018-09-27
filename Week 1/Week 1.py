88eb390aa1cd5732faafb247f5f92f6b19be02c5
Preising <klauss.preising@uconn.edu>
2018-09-27 10:05:18 -0400

    Weekly Challenges 1-3

---
+++ Week 1/Week 1.py
@@ -0,0 +1,39 @@
+def factorial_recursion(n):
+    Error_Message = "Try using a positive number."
+    if n==0:
+        return 1
+    elif n<0:
+        return Error_Message
+    else:
+        return n * factorial_recursion(n-1)
+
+factorial_recursion(-5)
+factorial_recursion(1)
+factorial_recursion(5)
+
+
+def factorial_loop(n):
+    factorial = 1
+    Error_Message = "Try using a positive number."
+    if n==0:
+        return 1
+    elif n<0:
+        return Error_Message
+    else:
+        for i in range(n,0,-1):
+            factorial = factorial * i
+        return factorial
+
+factorial_loop(-5)
+factorial_loop(0) 
+factorial_loop(1)
+factorial_loop(5)
+import math
+
+def factorial_easy(n):
+    return math.factorial(n)
+
+factorial_loop(-5)
+factorial_easy(0)
+factorial_easy(1)
+factorial_easy(5)
---
+++ Week 3/Week 3.py
@@ -0,0 +1,5 @@
+def change(n):
+    coins =  [0.5, 0.25, 0.1, 0.05, 0.01]
+
+    for i in coins:
+        pass
