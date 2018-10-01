ba1401f67c6c8ce9e2e058dff9638a84eb9d9514
kmp16101 <klauss.preising@uconn.edu>
2018-10-01 13:34:12 -0400

    Week 4

--- Week 3/Week 3.py
+++ Week 3/Week 3.py
@@ -6,12 +6,17 @@ Given this list [25, 50, 10, 5, 1] of coins find the least amount of coins it wo
 Example:
 201 would be 5 coins because 4 50 cent coins and 1 1 cent coins
 23 would be 5 coins because 2 30 cent coins and 3 1 cent coins
-
 """
 
 def change(n):
     coins = [50, 25, 10, 5, 1]
-
+    count = 0
+    for i in coins:
+        if n == 0:
+            break
+        count += (n // i)
+        n -= (n // i) * i
+    return count
 
 for i in range(5):
     temp = int(1000*random())
