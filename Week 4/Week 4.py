f24d79b94cc781e039f971ea79823fd7e9fae500
kmp16101 <klauss.preising@uconn.edu>
2018-10-03 23:21:01 -0400

    Edit week 3

---
+++ Week 4/Week 4.py
@@ -0,0 +1,10 @@
+"""
+Make a function that takes a list and returns the list sorted using selection sor
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+"""
+
+def selectionSort(lst):
+    pass
+
+selectionSort([2,1,54,5,345,3,45,13,21,3])
+selectionSort([0,2,89,3,6,57,65,76,57,653,4546,43])
