3d455872264f0e6787ca6eea98fbdac76251a18c
Klauss Preising <klauss.preising@gmail.com>
2018-10-08 13:02:50 -0400

    Week 4 Solution

--- Week 4/Week 4.py
+++ Week 4/Week 4.py
@@ -4,8 +4,15 @@ The selection sort algorithm sorts an array by repeatedly finding the minimum el
 The algorithm maintains two subarrays in a given array.
 """
 
-def selectionSort(lst):
-    pass
+def selectionSort(unsortedList):
+    lst = unsortedList[::]
+    for i in range(len(lst)):
+        temp = i
+        for j in range(i, len(lst)):
+            if lst[temp] > lst[j]:
+                temp = j
+        lst[i], lst[temp] = lst[temp], lst[i]
+    return lst
 
-selectionSort([2,1,54,5,345,3,45,13,21,3])
-selectionSort([0,2,89,3,6,57,65,76,57,653,4546,43])
+print(selectionSort([2,1,54,5,345,3,45,13,21,3]))
+print(selectionSort([0,2,89,3,6,57,65,76,57,653,4546,43]))
