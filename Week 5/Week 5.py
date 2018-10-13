bdb3b7688981b38a19ca438c6650c6b72703d19e
kmp16101 <klauss.preising@uconn.edu>
2018-10-12 21:28:45 -0400

    Week 5

---
+++ Week 5/Week 5.py
@@ -0,0 +1,13 @@
+"""
+Weekly Challenge Week 5:
+I want you guys to sort sort a list using Insertion sort
+Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time by putting the element in the proper position
+"""
+
+
+def insertionSort(x):
+    pass
+
+
+print(insertionSort([2, 1, 54, 5, 345, 3, 45, 13, 21, 3]))
+print(insertionSort([0, 2, 89, 3, 6, 57, 65, 76, 57, 653, 4546, 43]))
