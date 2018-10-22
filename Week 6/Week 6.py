e42dd83be697c86ada442f50d7430a0560b924c2
kmp16101 <klauss.preising@uconn.edu>
2018-10-22 13:25:36 -0400

    Week 6

---
+++ Week 6/Week 6.py
@@ -0,0 +1,16 @@
+"""
+Weekly Challenge week 6:
+I want you guys to sort a list using Bubble Sort.
+Bubble sort, compares each pair of adjacent items and swaps them if they are in the wrong order.
+"""
+
+def bubbleSort(unsorted_lst):
+    lst = unsorted_lst[::]
+    for i in range(1, len(lst)):
+        for j in range(i):
+            if lst[j] < lst[j-1]:
+                lst[j], lst[j-1] = lst[j-1], lst[j]
+    return lst
+
+print(bubbleSort([6,7,4,1,2,5,3]))
+print(bubbleSort([0, 2, 89, 3, 6, 57, 65, 76, 57, 653, 4546, 43]))
