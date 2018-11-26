461d60aed0e13b5572cad99ba09d0a0470059063
Klauss Preising <klauss.preising@gmail.com>
2018-11-26 15:52:31 -0500

    Solution number 7

--- Number 7/Number_7.py
+++ Number 7/Number_7.py
@@ -1,3 +1,10 @@
+"""
+Weekly Challenge:
+Write a function that rotates a list by k elements.
+For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2].
+"""
+
+
 def rotate(lst, n):
    if n > len(lst):
       n = n % len(lst)
@@ -5,4 +12,6 @@ def rotate(lst, n):
    y = lst[:n:]
    return x+y
 
+
 rotate([1, 2, 3, 4, 5, 6], 2)
+rotate([1, 2, 3, 4, 5, 6], 4)
