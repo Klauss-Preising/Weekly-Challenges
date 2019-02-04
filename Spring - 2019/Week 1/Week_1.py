67d7c86cde48a4dd6794d84a1b77a607650007ca
klausspreising <klauss.preising@uconn.edu>
2019-02-04 09:07:01 -0500

    Reformatting directories + week 1 of spring 2019

---
+++ Spring - 2019/Week 1/Week_1.py
@@ -0,0 +1,12 @@
+"""
+Week 1:
+For each multiple of n in the given array, change all the values following it to be that multiple of n, until encountering another multiple of n.
+
+Example:
+multiple(10, [2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+multiple(5, [10, 1, 20, 2]) → [10, 10, 20, 20]
+multiple(3, [10, 1, 9, 20]) → [10, 1, 9, 9]
+"""
+
+def foo(n, lst):
+    pass
