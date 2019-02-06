d19508fe0f2996c22a4e9aa0cfccb90b15948fde
klausspreising <klauss.preising@uconn.edu>
2019-02-06 06:27:39 -0500

    Week 2 spring 2019

---
+++ Spring - 2019/Week 2/Week 2.py
@@ -0,0 +1,16 @@
+"""
+Week 2:
+Given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner appear in outer.
+The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order.
+
+linearIn([1, 2, 4, 6], [2, 4]) → true
+linearIn([1, 2, 4, 6], [2, 3, 4]) → false
+linearIn([1, 2, 4, 4, 6], [2, 4]) → true
+"""
+
+def linearIn(lst1, lst2):
+    pass
+
+print(linearIn([1, 2, 4, 6], [2, 4]))
+print(linearIn([1, 2, 4, 6], [2, 3, 4]))
+print(linearIn([1, 2, 4, 4, 6], [2, 4]))
