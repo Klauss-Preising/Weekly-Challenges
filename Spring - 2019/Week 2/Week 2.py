4c98675ff83fc79841149cb80426703a54bb902c
klausspreising <klauss.preising@uconn.edu>
2019-02-19 16:50:42 -0500

    Week 2 spring 2019 - solution

--- Spring - 2019/Week 2/Week 2.py
+++ Spring - 2019/Week 2/Week 2.py
@@ -8,9 +8,48 @@ linearIn([1, 2, 4, 6], [2, 3, 4]) → false
 linearIn([1, 2, 4, 4, 6], [2, 4]) → true
 """
 
-def linearIn(lst1, lst2):
-    pass
 
-print(linearIn([1, 2, 4, 6], [2, 4]))
-print(linearIn([1, 2, 4, 6], [2, 3, 4]))
-print(linearIn([1, 2, 4, 4, 6], [2, 4]))
+# function by Klauss
+def linearIn_klauss(lst1, lst2):
+    j = 0
+    i = 0
+    while(j >= len(lst2) and i > len(lst1)):
+        if lst1[i] > lst2[j]:
+            return False
+        if lst1[i] == lst2[j]:
+            j += 1
+        i += 1
+    return j >= len(lst2)
+
+# function by David
+def linearIn_david(outer, inner):
+    if (all(i in outer for i in inner)):
+        return True
+    else:
+        return False
+
+# function by George
+def linearIn_george(outer, inner):
+    if len(inner) == 0:
+        print(True)
+    elif len(outer) == 0:
+        print(False)
+    elif inner[0] == outer[0]:
+        linearIn_george(outer[1:], inner[1:])
+    else:
+        linearIn_george(outer[1:], inner)
+
+# Function by Thahera
+def linearIn_thahera(outer, inner):
+    ans = True
+    outLen = len(outer)
+    inLen = len(inner)
+    if inLen > outLen:
+        return False
+    for element in inner:
+        if element in outer:
+            ans = True
+        else:
+            ans = False
+            break
+    return ans
