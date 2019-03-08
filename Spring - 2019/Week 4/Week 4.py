5a17650f88930e1a2bfadff002ea080b32b4eefe
klausspreising <klauss.preising@uconn.edu>
2019-03-08 09:33:05 -0500

    week 4 spring 2019

---
+++ Spring - 2019/Week 4/Week 4.py
@@ -0,0 +1,13 @@
+"""
+Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
+
+For example:
+
+turn on 0,0 through 999,999 would turn on (or leave on) every light.
+toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
+turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
+
+How many lights are on?
+"""
+
+
